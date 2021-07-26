db5172cd295db831376273b3c2f4872d9ef374e5
Toseefhusen <toseefhusen@webteamcorp.com>
2021-07-26 14:04:08 +0530

    Initial Commit

---
+++ GithubSearchUI/GithubSearchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchUIApp.swift
+//  GithubSearchUI
+//
+//  Created by Toseef on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
